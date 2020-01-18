2a0c7c58877ffc74a67e93acda015a6c7719a46c
RinkuAkash <akashrinku2@outlook.com>
2020-01-18 11:15:57 +0530

    Added: Solution using case statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,18 +5,22 @@ echo "Welcome to Employee Wage Computation Program"
 
 employeeType=$(($$%3))
 
-if [ $employeeType -eq 1 ]; then
-	echo "Employee Present"
-elif [ $employeeType -eq 2 ]; then
-	echo "Part Time Employee Present"
-else
-	echo "Employee Absent"
-	exit 1
-fi
-
 WAGEPERHOUR=20
 WORKINGHOURS=8
-if [ $employeeType -eq 1 ] || [ $employeeType -eq 2 ]; then
-	dailyWage=$(($WAGEPERHOUR * $WORKINGHOURS))
-	echo "Employee daily wage is ${dailyWage}"
-fi
+
+case $employeeType in
+	1)
+		echo "Employee Present"
+		dailyWage=$(($WAGEPERHOUR * $WORKINGHOURS))
+		echo "Employee Daily Wage is ${dailyWage}"
+		;;
+	2)
+		echo "Part Time Employee Present"
+		dailyWage=$(($WAGEPERHOUR * $WORKINGHOURS))
+		echo "Employee Daily Wage is ${dailyWage}"
+		;;
+	*)
+		echo "Employee Absent"
+		exit 1
+		;;
+esac
