6c6f4b9a235889f6b8642a6f888d1d2f0aaad65c
RinkuAkash <akashrinku2@outlook.com>
2020-01-18 10:39:07 +0530

    Added: Program to check whether employee present or not

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,3 +2,14 @@
 
 echo "Welcome to Employee Wage Computation Program"
 
+
+employeeType=$(($$%3))
+
+if [ $employeeType -eq 1 ]; then
+	echo "Employee Present"
+elif [ $employeeType -eq 2 ]; then
+	echo "Part Time Employee Present"
+else
+	echo "Employee Absent"
+fi
+
