a451f342fcf0f621fb019fce36487bb3b4e079b6
RinkuAkash <akashrinku2@outlook.com>
2020-01-18 11:25:30 +0530

    Added: Program to calculate employee monthly wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,17 +7,20 @@ employeeType=$(($$%3))
 
 WAGEPERHOUR=20
 WORKINGHOURS=8
+DAYSPERMONTH=20
 
 case $employeeType in
 	1)
 		echo "Employee Present"
 		dailyWage=$(($WAGEPERHOUR * $WORKINGHOURS))
 		echo "Employee Daily Wage is ${dailyWage}"
+		echo "Employee Monthly Wage is "$(($dailyWage * $DAYSPERMONTH))
 		;;
 	2)
 		echo "Part Time Employee Present"
 		dailyWage=$(($WAGEPERHOUR * $WORKINGHOURS))
 		echo "Employee Daily Wage is ${dailyWage}"
+		echo "Employee Monthly Wage is "$(($dailyWage * $DAYSPERMONTH))
 		;;
 	*)
 		echo "Employee Absent"
